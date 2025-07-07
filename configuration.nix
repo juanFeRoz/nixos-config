d5d6b56958def3e9504e9b689bcaa1bdce2dde47
juferoz <juanfeliperojas2@protonmail.com>
2025-07-07 18:11:45 -0500

    small fix

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
       };
     };
   };
