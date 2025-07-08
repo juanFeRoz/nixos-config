66adf77c4fea6fa3d11e6768457a8c7144545047
juferoz <juanfeliperojas2@protonmail.com>
2025-07-08 09:48:51 -0500

    added kitty

--- configuration.nix
+++ configuration.nix
@@ -1,102 +1,103 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
+config,
+lib,
+pkgs,
+...
 }: {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  boot.loader.systemd-boot.configurationLimit = 10;
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 1w";
-  };
-
-  nix.settings.auto-optimise-store = true;
-
-  boot.loader.systemd-boot.enable = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "America/Bogota";
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  users.users.juanfe = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    packages = with pkgs; [
-      git
-      alejandra
-      adwaita-icon-theme
-      glib
-      waybar
-      firefox
-      pwvucontrol
-      gccgo15
-      tree-sitter
-      fzf
-      networkmanagerapplet
-      wlprop
-      unzip
-      slurp
-      wl-clipboard
-      mako
-      tmux
-      xdg-user-dirs
-      pw-volume
-      playerctl
-      ripgrep
-      alacritty
+    imports = [
+        ./hardware-configuration.nix
     ];
-  };
 
-  fonts.enableDefaultPackages = true;
+    boot.loader.systemd-boot.configurationLimit = 10;
 
-  programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-  ];
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-  ];
-
-  services.gnome.gnome-keyring.enable = true;
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [grim swayidle swaylock brightnessctl wmenu ];
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-      };
+    nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 1w";
     };
-  };
 
-  environment.systemPackages = with pkgs; [
-    neovim
-  ];
+    nix.settings.auto-optimise-store = true;
 
-  system.stateVersion = "25.05";
+    boot.loader.systemd-boot.enable = true;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    networking.hostName = "nixos";
+    networking.networkmanager.enable = true;
+
+    time.timeZone = "America/Bogota";
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+    };
+
+    users.users.juanfe = {
+        isNormalUser = true;
+        extraGroups = ["wheel"];
+        packages = with pkgs; [
+            git
+            alejandra
+            adwaita-icon-theme
+            glib
+            waybar
+            firefox
+            pwvucontrol
+            gccgo15
+            tree-sitter
+            fzf
+            networkmanagerapplet
+            wlprop
+            unzip
+            slurp
+            wl-clipboard
+            mako
+            tmux
+            xdg-user-dirs
+            pw-volume
+            playerctl
+            ripgrep
+            alacritty
+            kitty
+        ];
+    };
+
+    fonts.enableDefaultPackages = true;
+
+    programs.thunar.enable = true;
+    programs.thunar.plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+    ];
+    services.gvfs.enable = true; # Mount, trash, and other functionalities
+
+    fonts.packages = with pkgs; [
+        nerd-fonts.fira-code
+        nerd-fonts.droid-sans-mono
+    ];
+
+    services.gnome.gnome-keyring.enable = true;
+
+    programs.sway = {
+        enable = true;
+        wrapperFeatures.gtk = true;
+        extraPackages = with pkgs; [grim swayidle swaylock brightnessctl wmenu ];
+    };
+
+    services.greetd = {
+        enable = true;
+        settings = {
+            default_session = {
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+            };
+        };
+    };
+
+    environment.systemPackages = with pkgs; [
+        neovim
+    ];
+
+    system.stateVersion = "25.05";
 }
