6cfb95344f384b2d7fdb8fe637d01372a440f3a5
juferoz <juanfeliperojas2@protonmail.com>
2025-07-08 10:50:25 -0500

    added direnv

--- configuration.nix
+++ configuration.nix
@@ -61,18 +61,8 @@ pkgs,
             ripgrep
             alacritty
             kitty
+            direnv
         ];
-    };
-
-    programs.direnv = {
-        package = pkgs.direnv;
-        silent = false;
-        loadInNixShell = true;
-        direnvrcExtra = "";
-        nix-direnv = {
-            enable = true;
-            package = pkgs.nix-direnv;
-        };
     };
 
     fonts.enableDefaultPackages = true;
