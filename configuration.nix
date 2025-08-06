8bf8286a19d9e4d9c690bd7162877d3bd6119092
juferoz <juanfeliperojas2@protonmail.com>
2025-08-06 14:03:53 -0500

    added some powersaving tools

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,21 @@ pkgs,
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
 
+    powerManagement.powertop.enable = true;
+    services = {
+        power-profiles-daemon.enable = false;
+        tlp = {
+            enable = true;
+            settings = {
+                CPU_BOOST_ON_AC = 1;
+                CPU_BOOST_ON_BAT = 0;
+                CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+                STOP_CHARGE_THRESH_BAT0 = 95;
+            };
+        };
+    };
+
     services.gvfs.enable = true; # Mount, trash, and other functionalities
 
     hardware.bluetooth.enable = true; # enables support for Bluetooth
