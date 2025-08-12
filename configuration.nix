766e7b0a50d777c2600328a1fcff500f17dcda38
juferoz <juanfeliperojas2@protonmail.com>
2025-08-12 10:25:56 -0500

    try to solve connection problems

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,8 @@ pkgs,
 
     networking.hostName = "nixos";
     networking.networkmanager.enable = true;
+    networking.wireless.iwd.enable = true;
+    networking.networkmanager.wifi.backend = "iwd";
 
     time.timeZone = "America/Bogota";
 
