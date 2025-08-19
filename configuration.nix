c3326f2fc3aec7f36e5a8d7ef1e5aef043cd2f11
juferoz <juanfeliperojas2@protonmail.com>
2025-08-19 17:13:58 -0500

    added plymouth

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,18 @@ pkgs,
 
     boot.plymouth.enable = true; 
 
+    boot = {
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        kernelParams = [
+            "quiet"
+            "splash"
+            "boot.shell_on_fail"
+            "udev.log_priority=3"
+            "rd.systemd.show_status=auto"
+        ];
+    }
+
     time.timeZone = "America/Bogota";
 
     nix.settings.experimental-features = ["nix-command" "flakes"];
