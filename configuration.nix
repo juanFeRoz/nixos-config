c5569e4af99d2ee5db19476cb1d6a0633687f889
juferoz <juanfeliperojas2@protonmail.com>
2025-08-19 17:05:23 -0500

    added plymouth

--- configuration.nix
+++ configuration.nix
@@ -31,6 +31,35 @@ pkgs,
     services.resolved.enable = true;
 
 
+    boot = {
+        plymouth = {
+            enable = true;
+            theme = "rings";
+            themePackages = with pkgs; [
+                # By default we would install all themes
+                (adi1090x-plymouth-themes.override {
+                    selected_themes = [ "rings" ];
+                })
+            ];
+        };
+
+        # Enable "Silent boot"
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        kernelParams = [
+            "quiet"
+            "splash"
+            "boot.shell_on_fail"
+            "udev.log_priority=3"
+            "rd.systemd.show_status=auto"
+        ];
+        # Hide the OS choice for bootloaders.
+        # It's still possible to open the bootloader list by pressing any key
+        # It will just not appear on screen unless a key is pressed
+        loader.timeout = 0;
+
+    };
+
     time.timeZone = "America/Bogota";
 
     nix.settings.experimental-features = ["nix-command" "flakes"];
