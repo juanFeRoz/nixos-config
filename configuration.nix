dc34af51e6da34ceb03ed711939abe8742594840
juferoz <juanfeliperojas2@protonmail.com>
2025-09-22 10:38:10 -0500

    update

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@ pkgs,
             lua-language-server
             obsidian
             nwg-look
-            apvlv
+            fancy-cat
             docker-compose
             file-roller
             gccgo15
